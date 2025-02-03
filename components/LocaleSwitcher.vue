fa02b8306ae9436cc3b406cb377d9447a4acc0be
Kidus Abebe <kidsebk@gmail.com>
2025-02-03 22:05:27 +0300

    add: header components and layout

---
+++ components/LocaleSwitcher.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useI18n } from 'vue-i18n'
+
+const { locale, availableLocales } = useI18n()
+
+const switchLocale = (newLocale) => {
+  locale.value = newLocale
+}
+</script>
+
+<template>
+  <select @change="switchLocale($event.target.value)" :value="locale">
+    <option v-for="loc in availableLocales" :key="loc" :value="loc">
+      {{ loc }}
+    </option>
+  </select>
+</template> 
---
+++ components/layout/Header.vue
@@ -0,0 +1,28 @@
+<template>
+	<!-- Navigation -->
+	<nav class="bg-[#121521] border-b border-white/10 px-6 py-4">
+		<div class="max-w-7xl mx-auto flex items-center justify-between">
+			<div class="flex items-center gap-8">
+				<a href="/" class="flex items-center gap-2">
+					<img src="https://nuxt.com/assets/design-kit/logo/icon-green.svg" alt="Nuxt"
+						class="h-8 w-8" />
+					<span class="font-semibold text-xl text-[#ebdbb2]">Jemmo</span>
+				</a>
+				<div class="hidden md:flex items-center m-3 gap-6">
+					<a href="#" class="hover:text-[#00DC82]">Shops</a>
+					<a href="#" class="hover:text-[#00DC82]">Projects</a>
+					<a href="#" class="hover:text-[#00DC82]">Enterprise</a>
+					<a href="#" class="hover:text-[#00DC82]">About</a>
+				</div>
+			</div>
+			<div class="flex items-center gap-4">
+				<button class="p-2 hover:bg-white/10 rounded-full">
+					<!-- <IconSearch class="w-5 h-5" /> -->
+				</button>
+				<button class="p-2 hover:bg-white/10 rounded-full">
+					<!-- <IconMoon class="w-5 h-5" /> -->
+				</button>
+			</div>
+		</div>
+	</nav>
+</template>
