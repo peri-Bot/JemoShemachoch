dcf0f2374bb847d0dfa1284598d170bbc226ac2c
Kidus Abebe <kidsebk@gmail.com>
2025-02-03 22:05:50 +0300

    add: default layout

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<!-- layouts/default.vue -->
+<template>
+	<div class="min-h-screen bg-[#0e2332] text-white">
+		<Header />
+		<main>
+			<slot />
+		</main>
+	</div>
+</template>
+
+<script setup>
+// Auto-imported components
+</script>
