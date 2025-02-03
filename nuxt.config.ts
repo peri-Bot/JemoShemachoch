c5c6d265d825bcc41b07c7377f7f9e81434f4bce
Kidus Abebe <kidsebk@gmail.com>
2025-02-03 22:04:48 +0300

    chore: packages and components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,40 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/i18n',
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/color-mode'
-  ],
-  i18n: {
-    locales: ['en', 'am'],
-    defaultLocale: 'en',
-    strategy: 'prefix_except_default',
-    vueI18n: './i18n.config.ts'
-  },
-  colorMode: {
-    classSuffix: '',
-    preference: 'light',
-    fallback: 'light'
-  },
-  tailwindcss: {
-    config: {
-      content: [],
-      theme: {
-        extend: {
-          colors: {
-            gruvbox: {
-              dark_hard: '#1d2021',
-              dark0: '#282828',
-              // ... rest of gruvbox colors from user input
-            }
-          }
-        }
-      }
-    }
-  }
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	modules: [
+		'@nuxtjs/i18n',
+		'@nuxtjs/tailwindcss',
+		'@nuxtjs/color-mode'
+	],
+	components: [
+		'~/components/layout',
+	],
+	i18n: {
+		locales: ['en', 'am'],
+		defaultLocale: 'en',
+		strategy: 'prefix_except_default',
+		vueI18n: './i18n.config.ts'
+	},
+	colorMode: {
+		classSuffix: '',
+		preference: 'light',
+		fallback: 'light'
+	},
+	tailwindcss: {
+		config: {
+			content: [],
+			theme: {
+				extend: {
+					colors: {
+						gruvbox: {
+							dark_hard: '#1d2021',
+							dark0: '#282828',
+							// ... rest of gruvbox colors from user input
+						}
+					}
+				}
+			}
+		}
+	}
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,28 @@ module.exports = {
 	],
 	theme: {
 		extend: {
-			colors: ({ theme }) => ({
-				gruvbox: theme('colors.gruvbox'),
-			}),
+			colors: {
+				gruvbox: {
+					bg: '#282828',
+					bg0_h: '#1d2021',
+					bg0: '#282828',
+					bg1: '#3c3836',
+					bg2: '#504945',
+					bg3: '#665c54',
+					fg: '#ebdbb2',
+					fg0: '#fbf1c7',
+					fg1: '#ebdbb2',
+					fg2: '#d5c4a1',
+					fg3: '#bdae93',
+					red: '#fb4934',
+					green: '#b8bb26',
+					yellow: '#fabd2f',
+					blue: '#83a598',
+					purple: '#d3869b',
+					aqua: '#8ec07c',
+					orange: '#fe8019',
+				}
+			}
 		}
 	}
 } 
