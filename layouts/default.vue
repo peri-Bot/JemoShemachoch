0e0a12fc8f30072efd976c637e99183297631ad6
kidsebk <kidsebk@gmail.com>
2025-04-08 23:14:14 +0000

    feat(ui): add lamp container

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,16 @@
-<!-- layouts/default.vue -->
 <template>
-	<div class="min-h-screen bg-[#0e2332] text-white">
+	<div
+		class="min-h-screen font-sans bg-light-bg dark:bg-dark-bg text-light-text dark:text-dark-text transition-colors duration-300">
+		<LampContainer lineWidth="60%" glowHeight="35rem" glowBlur="50px"
+			containerClass="absolute inset-x-0 top-[4rem] w-full h-[40rem] z-5 overflow-hidden" />
 		<Header />
 		<main>
 			<slot />
 		</main>
+		<Footer />
 	</div>
 </template>
 
 <script setup>
-// Auto-imported components
+// No script needed if just using auto-imported components
 </script>
