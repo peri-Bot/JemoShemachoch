38a732c2e461094e1b41acd1d613f0fdd6f1e37c
kidsebk <kidsebk@gmail.com>
2025-04-08 23:13:25 +0000

    feat(config): add langs

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,14 +1,5 @@
 export default defineI18nConfig(() => ({
-  legacy: false,
-  locale: 'en',
-  messages: {
-    en: {
-      welcome: 'Welcome',
-      // Add all English translations
-    },
-    am: {
-      welcome: 'እንኳን ደህና መጡ',
-      // Add all Amharic translations
-    }
-  }
+	legacy: false,
+	locale: 'en',
+	fallbackFormat: 'en',
 })) 
