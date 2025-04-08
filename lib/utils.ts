b00151b3cc913d770bde3098add9528cea04d5de
kidsebk <kidsebk@gmail.com>
2025-04-08 23:46:47 +0000

    add utils

---
+++ lib/utils.ts
@@ -0,0 +1,9 @@
+// src/lib/utils.ts
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs))
+}
+
+// You can add other utility functions here if needed
