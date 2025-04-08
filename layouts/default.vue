4e708e82b095fcda44c3dd68ac408158a7297a89
kidsebk <kidsebk@gmail.com>
2025-04-08 23:23:47 +0000

    refactor(ui): removed LampContainer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,6 @@
 <template>
 	<div
 		class="min-h-screen font-sans bg-light-bg dark:bg-dark-bg text-light-text dark:text-dark-text transition-colors duration-300">
-		<LampContainer lineWidth="60%" glowHeight="35rem" glowBlur="50px"
-			containerClass="absolute inset-x-0 top-[4rem] w-full h-[40rem] z-5 overflow-hidden" />
 		<Header />
 		<main>
 			<slot />
