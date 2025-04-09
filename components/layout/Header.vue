06307a40f92e6437d4efaa5ba7010ade3dceeeb2
kidsebk <kidsebk@gmail.com>
2025-04-09 13:09:51 +0000

    ui(Header.vu): change company name color to text-secondory

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -11,8 +11,9 @@
 							<path
 								d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z" />
 						</svg>
-						<span class="font-bold text-xl text-light-text dark:text-dark-text">{{
-							$t('companyName') }}</span>
+						<span
+							class="font-bold text-xl text-light-text-secondary dark:text-dark-text-secondary">{{
+								$t('companyName') }}</span>
 					</NuxtLink>
 				</div>
 
@@ -93,7 +94,7 @@ const navigation = [
 
 /* Active link styling (NuxtLink adds 'router-link-active' or 'router-link-exact-active') */
 .router-link-exact-active {
-	@apply font-bold text-light-accent dark:text-dark-accent;
+	@apply font-bold text-light-text-secondary dark:text-dark-text-secondary;
 }
 
 .mobile-nav-link {
