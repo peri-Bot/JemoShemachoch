d53397e3bd085b93a92eff97ae0eb742486e5c96
kidsebk <kidsebk@gmail.com>
2025-04-11 18:50:37 +0000

    fix(config): migrate from nuxtjs/tailwindcss to tailwindcss/vite and tailwindcss v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
+	vite: {
+		plugins: [
+			tailwindcss(),
+		],
+	},
 	modules: [
 		'@nuxtjs/i18n',
-		'@nuxtjs/tailwindcss',
+		// '@nuxtjs/tailwindcss',
+		'@nuxt/ui',
 		'@nuxtjs/color-mode',
 		'@nuxtjs/google-fonts',
 		'@vueuse/motion/nuxt'
